c990b3601bb8f42da8b628a7eef6b9e0e00970ed
Amol Kobal <amol_kobal@rediffmail.com>
2020-06-21 20:07:16 +0530

    Date Time validations for new Gig

---
+++ ViewModel/DateValidation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace GigHub.ViewModel
+{
+    public class ValidDate : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime result;
+            var isValid = DateTime.TryParseExact(
+                Convert.ToString(value),
+                "d MMM yyyy",
+                CultureInfo.CurrentCulture,
+                DateTimeStyles.None,
+                out result);
+
+            return (isValid && result > DateTime.Now);
+        }
+    }
+
+    public class ValidTime : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime result;
+            var isValid = DateTime.TryParseExact(
+                Convert.ToString(value),
+                "HH:MM",
+                CultureInfo.CurrentCulture,
+                DateTimeStyles.None,
+                out result);
+
+            return (isValid);
+        }
+    }
+}
--- ViewModel/GigFormViewModel.cs
+++ ViewModel/GigFormViewModel.cs
@@ -1,25 +1,31 @@
 ﻿using GigHub.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GigHub.ViewModel
 {
     public class GigFormViewModel
     {
+        [Required]
         public string Venue { get; set; }
+
+        [Required]
+        [ValidDate]
         public string Date { get; set; }
+
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
 
+        [Required]
         public byte Genre { get; set; }
 
         public IEnumerable<Genre> Genres { get; set; }
 
-        public DateTime DateTime
+        public DateTime Schedule()
         {
-            get
-            {
-                return DateTime.Parse($"{Date} {Time}");
-            }
+            return DateTime.Parse($"{Date} {Time}");
         }
     }
 }
