cfce8d6f6c1a39c8107670afc65701b62fc01e26
Amol Kobal <amol_kobal@rediffmail.com>
2020-06-21 19:25:08 +0530

    Refactoring Controller code, Remove Name and add ForeignKey

--- Models/Gig.cs
+++ Models/Gig.cs
@@ -7,8 +7,10 @@ namespace GigHub.Models
     {
         public int Id { get; set; }
 
-        [Required]
         public ApplicationUser Artist { get; set; }
+
+        [Required]
+        public string ArtistId { get; set; }
         public DateTime Datetime { get; set; }
 
         [Required]
@@ -16,6 +18,8 @@ namespace GigHub.Models
         public string Venue { get; set; }
 
         [Required]
+        public byte GenreId { get; set; }
+
         public Genre Genre { get; set; }
     }
 }
